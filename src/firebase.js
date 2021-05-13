d8f077db7678a49e130bf1cce5c4fc084c5a8d9a
Ashish <ashishDongol32@gmail.com>
2021-05-13 09:11:38 +0545

    asd

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBuAMDQnMICXwmSRbWVtKhPK8QYlyGaaI",
+  authDomain: "clone-5a9f2.firebaseapp.com",
+  projectId: "clone-5a9f2",
+  storageBucket: "clone-5a9f2.appspot.com",
+  messagingSenderId: "941090612949",
+  appId: "1:941090612949:web:558c6e29f7223dd68d645c",
+  measurementId: "G-85J06XC00S"
+};
