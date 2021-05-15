560fd2b5873db17d2665456fd1fcf619c4a93fcc
Ashish <ashishDongol32@gmail.com>
2021-05-15 18:34:53 +0545

    header implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
-
+    // BEM Convention
+    <div className='App'>
+      {/* Header */}
+      <Header />
     </div>
+
+    // Home
   );
 }
 
