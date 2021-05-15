86d079ec0fd20832393d9c22623b6313d3ed6c63
Ashish <ashishDongol32@gmail.com>
2021-05-15 18:58:41 +0545

    progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Header from './component/Header';
+import Home from './Screen/Home';
 
 function App() {
   return (
     // BEM Convention
     <div className='App'>
-      {/* Header */}
       <Header />
+      <Home />
     </div>
-
-    // Home
   );
 }
 
